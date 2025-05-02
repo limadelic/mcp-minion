2fe63140163c0a913c80d8c2ee1fba361ea7de4b
msuarz <mikemps@gmail.com>
2025-05-02 13:30:33 -0400

    add:tool-schema-display

--- src/tools.ts
+++ src/tools.ts
@@ -13,6 +13,12 @@ export async function list(client) {
 }
 
 export async function call(client, name = argv.name, args = argv.args) {
+  if (Object.keys(args).length === 0) {
+    const tools = await client.listTools();
+    const tool = tools.tools.find(t => t.name === name);
+    if (tool) console.log(yaml.dump(tool));
+    return;
+  }
   (await client.callTool({ name, arguments: args })).content.forEach(item =>
     console.log(yaml.dump(item)),
   );
