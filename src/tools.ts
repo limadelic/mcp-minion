4ac048aadf7bcc714f1f029244807a0153ca8f95
msuarz <mikemps@gmail.com>
2025-05-02 13:31:43 -0400

    fix:no-args-tools

--- src/tools.ts
+++ src/tools.ts
@@ -16,9 +16,16 @@ export async function call(client, name = argv.name, args = argv.args) {
   if (Object.keys(args).length === 0) {
     const tools = await client.listTools();
     const tool = tools.tools.find(t => t.name === name);
-    if (tool) console.log(yaml.dump(tool));
-    return;
+
+    if (tool) {
+      const hasRequiredArgs = tool.inputSchema?.required?.length > 0;
+      if (hasRequiredArgs) {
+        console.log(yaml.dump(tool));
+        return;
+      }
+    }
   }
+
   (await client.callTool({ name, arguments: args })).content.forEach(item =>
     console.log(yaml.dump(item)),
   );
