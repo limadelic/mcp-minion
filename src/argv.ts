c082b5b6e076c9c6617cc0cc2b8e5cfe623a462e
msuarz <mikemps@gmail.com>
2025-05-02 12:57:30 -0400

    fix:mongodb-tools-command

--- src/argv.ts
+++ src/argv.ts
@@ -8,11 +8,19 @@ const cmds = ["help", "dir", "servers", "tools"];
 const isCmd = cmds.includes(arg);
 const isServer = servers.includes(arg);
 
-export const cmd = isCmd ? arg : !isServer ? "help" : "run";
+const isToolsCommand = params[1] === "tools";
+export const cmd = isCmd
+  ? arg
+  : !isServer
+    ? "help"
+    : isToolsCommand
+      ? "tools"
+      : "run";
 
 export const server = isServer ? arg : null;
 export const resource = "tools";
-export const name = isServer ? params[1] : null;
+export const name =
+  isServer && params[1] && params[1] !== "tools" ? params[1] : null;
 export const args = isServer && params[2] ? JSON.parse(params[2]) : {};
 
 export const argv = { cmd, server, resource, name, args };
