339d560ccaf3656fe7bb1c69c1b3b8ca362d097b
msuarz <mikemps@gmail.com>
2025-05-04 17:45:17 -0400

    refactor:inline-argv-getters

--- src/argv.js
+++ src/argv.js
@@ -2,39 +2,28 @@ import { servers } from "./conf.js";
 
 const argv = process.argv.slice(2);
 
-function getCmd() {
-  return ["help", "servers", "add"].includes(
-    argv[0],
-  )
-    ? argv[0]
-    : servers.includes(argv[0])
-      ? "run"
-      : "help";
-}
+export const cmd = [
+  "help",
+  "servers",
+  "add",
+].includes(argv[0])
+  ? argv[0]
+  : servers.includes(argv[0])
+    ? "run"
+    : "help";
 
-export const cmd = getCmd();
-
-function getServer() {
-  return cmd === "run" ? argv[0] : argv[1];
-}
-
-export const server = getServer();
+export const server =
+  cmd === "run" ? argv[0] : argv[1];
 
 export const resource = "tools";
 
-function getName() {
-  return cmd === "run" ? argv[1] : argv[2];
-}
+export const name =
+  cmd === "run" ? argv[1] : argv[2];
 
-export const name = getName();
-
-function getArgs() {
-  return cmd === "run"
+export const args =
+  cmd === "run"
     ? JSON.parse(argv[2] || "{}")
     : argv.slice(3);
-}
-
-export const args = getArgs();
 
 export default {
   cmd,
