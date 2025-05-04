e06fafefa83925c82d07d8c4ff58cf7bdaa25f1c
msuarz <mikemps@gmail.com>
2025-05-04 15:00:12 -0400

    cleanup:remove-typescript-files

--- src/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { help, servers } from "./help.js";
-import { run } from "./client.js";
-
-export default {
-  help,
-  servers,
-  run,
-};
--- src/oo.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import { cmd } from "./argv.js";
-import handlers from "./handlers.js";
-
-handlers[cmd]();
