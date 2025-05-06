1d01f5ae5faa42dcbffbef6313551d86a5311ff3
msuarz <mikemps@gmail.com>
2025-05-05 21:40:56 -0400

    refactor:extract logArray and logObject functions

--- src/log.js
+++ src/log.js
@@ -1,15 +1,19 @@
 import yaml from "js-yaml";
 import _ from "lodash";
 
-export function log(data, fields) {
-  if (_.isArray(data))
-    return _.each(data, item =>
-      log(item, fields),
-    );
+const logArray = (data, fields) =>
+  _.each(data, item => log(item, fields));
 
+const logObject = (data, fields) =>
   console.log(
     yaml.dump(
       fields ? _.pick(data, fields) : data,
     ),
   );
+
+export function log(data, fields) {
+  if (_.isArray(data)) logArray(data, fields);
+  else if (_.isObject(data))
+    logObject(data, fields);
+  else console.log(data);
 }
