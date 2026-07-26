3ec2fab95710d947d1cf4ff1ed2ef13cfa722664
msuarz <mikemps@gmail.com>
2026-07-26 19:31:24 -0400

    add:http transport support

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
 import { mcpServers } from "./conf.js";
 import * as tools from "./tools.js";
 import * as argv from "./argv.js";
@@ -15,13 +17,36 @@ export async function run(
     { capabilities: {} },
   );
 
-  const transport = new StdioClientTransport({
-    ...mcpServers[server],
-    env: process.env,
-  });
+  const config = mcpServers[server];
+  const transport = config.url
+    ? await createHttpTransport(config)
+    : new StdioClientTransport({
+        ...config,
+        env: process.env,
+      });
+
   await client.connect(transport);
   await ((name &&
     tools.call(client, name, args)) ||
     tools.list(client));
   await client.close();
+}
+
+async function createHttpTransport(config) {
+  const requestInit = {};
+  if (config.headers) {
+    requestInit.headers = config.headers;
+  }
+
+  try {
+    return new StreamableHTTPClientTransport({
+      url: new URL(config.url),
+      requestInit,
+    });
+  } catch {
+    return new SSEClientTransport({
+      url: new URL(config.url),
+      requestInit,
+    });
+  }
 }
