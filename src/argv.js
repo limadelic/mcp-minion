6140880b6ee0b98e11bdeb6e1375a94a3b69581a
msuarz <mikemps@gmail.com>
2026-07-26 19:31:05 -0400

    add:url detection in argv

--- src/argv.js
+++ src/argv.js
@@ -18,15 +18,40 @@ export const resource = "tools";
 
 export const name = argv[cmd === "run" ? 1 : 2];
 
+const isUrl = name?.match(/^https?:\/\//);
+
+export const url = isUrl ? name : null;
+
+export const headers =
+  !isUrl && cmd === "add"
+    ? parseHeaders(argv.slice(3))
+    : {};
+
 export const args =
   cmd === "run"
     ? JSON.parse(argv[2] || "{}")
-    : argv.slice(3);
+    : isUrl
+      ? {}
+      : argv.slice(3);
+
+function parseHeaders(args) {
+  const headers = {};
+  for (let i = 0; i < args.length; i += 2) {
+    if (args[i].startsWith("--")) {
+      const key = args[i].slice(2);
+      const value = args[i + 1];
+      if (value) headers[key] = value;
+    }
+  }
+  return headers;
+}
 
 export default {
   cmd,
   server,
   resource,
   name,
+  url,
+  headers,
   args,
 };
